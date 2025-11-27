7ee62d2f27b72a52191159fc711403715eda8527
aragoza <12339@holbertonstudents.com>
2025-11-27 22:08:48 +0100

    i have change the void * type to va_list to avoid crash and error

--- test_printf/get_percent_func.c
+++ test_printf/get_percent_func.c
@@ -12,23 +12,23 @@
  * the % identified
  */
 
-void (*get_percent_func)(char *letter_percent)
+char *(*get_percent_func(char letter_percent))(va_list)
 {
 	percent_func_type func_and_type[] = {
-        {"c", f_char},
-        {"s", f_string},
-        {"d", f_int},
-        {"f", f_float},
-        {NULL, NULL}
-    };
+		{'c', f_char},
+		{'s', f_string},
+		{'d', f_int},
+		/*{'f', f_float},*/
+		{'\0', NULL}
+	};
 
-    int i;
-    
-    for(i = 0, letter_percent[0] != NULL, i++)
-    {
-        if(func_and_type[i].percent == letter_percent[0])
-            return (func_and_type[i].function_percent);
-    }
+	int i;
+	
+	for(i = 0; func_and_type[i].percent != '\0'; i++)
+	{
+		if(func_and_type[i].percent == letter_percent)
+			return (func_and_type[i].function_percent);
+	}
 
-    return (NULL);
+	return (NULL);
 }
