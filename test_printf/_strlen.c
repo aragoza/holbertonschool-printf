bb84b92bfce265aaf6584f14239b32ca16347ace
aragoza <12339@holbertonstudents.com>
2025-11-27 22:06:02 +0100

    re-add the file _strlen because i choose to do malloc and i need it for the f_string function

---
+++ test_printf/_strlen.c
@@ -0,0 +1,19 @@
+#include "test_header.h"
+
+/**
+ * _strlen - say the size of a string
+ *
+ * @s: is the string parameter
+ *
+ * Return: (i) the size of s
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
